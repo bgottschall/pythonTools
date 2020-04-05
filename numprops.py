57bcf81b8f168d015fa22ec770bd09e43455888d
Björn Gottschall <git.mail@bgottschall.de>
2020-04-05 11:39:47 +0200

    * removed p-value.py as this feature is merged into numprops.py

--- numprops.py
+++ numprops.py
@@ -2,7 +2,6 @@
 
 from argparse import ArgumentParser
 import numpy
-import sys
 
 
 def isFloat(val):
@@ -26,7 +25,8 @@ props = {
     'p%': {'label': 'P%', 'func': lambda x, y: numpy.percentile(x, y)},
     'avg': {'label': 'Avg', 'func': numpy.mean},
     'std': {'label': 'σ', 'func': numpy.std},
-    'var': {'label': 'σ²', 'func': numpy.var}
+    'var': {'label': 'σ²', 'func': numpy.var},
+    'pval': {'label': 'P-Value', 'func': lambda x, y: stats.ttest_ind(x, y, equal_var=False)[1]}
 }
 
 defaultProps = ['count', 'sum', 'min', 'max', 'q2', 'avg', 'std']
@@ -34,48 +34,60 @@ defaultProps = ['count', 'sum', 'min', 'max', 'q2', 'avg', 'std']
 parser = ArgumentParser(description="output number properties from numbers passed or read from stdin")
 parser.add_argument("--stdin", help=f"read from stdin even if numbers are provided", default=False, action="store_true")
 parser.add_argument("--precision", help=f"force a specific precision", type=int, default=None)
-parser.add_argument("-p", "--properties", help=f"format output (default {', '.join(defaultProps)}) (valid {', '.join(list(props.keys())).replace('%','%%')})", nargs="+", default=defaultProps)
+parser.add_argument("-p", "--properties", help=f"format output (default {', '.join(defaultProps)}) (valid {', '.join(list(props.keys())).replace('%','%%')})", type=str.lower, nargs="+", default=defaultProps)
 parser.add_argument("-q", "--quiet", help="minimal output", default=False, action="store_true")
-parser.add_argument("numbers", help="numbers to calculate properties on (default read from stdin)", default=None, nargs='*')
+parser.add_argument("--others", help="other number set used e.g. to calculate p-value statistics", default=[], nargs='*')
+parser.add_argument("primary", help="numbers to calculate properties on (default read from stdin)", default=[], nargs='*')
 args = parser.parse_args()
 
-if args.numbers is None or len(args.numbers) == 0:
-    args.stdin = True
-    args.numbers = []
+pValue = False
+if 'pval' in args.properties:
+    # One number set can be read from stdin, if both primary and other is empty we cannot calculate p-value statistics
+    if len(args.primary) == 0 and len(args.others) == 0:
+        raise Exception('two number sets are required to calculate p-value statistics, provide them via primary and --others or stdin')
+    # One number set is provided, read the other one from stdin
+    if len(args.primary) == 0 or len(args.others) == 0:
+        args.stdin = True
+    from scipy import stats
+    pValue = True
 
 if args.stdin:
+    import sys
+    stdinSet = []
     for line in sys.stdin.readlines():
-        args.numbers.extend(line.split())
+        stdinSet.extend(line.split())
+    if pValue and len(args.others) == 0:
+        args.others = stdinSet
+    else:
+        args.primary.extend(stdinSet)
 
-numbers = numpy.array([float(x) for x in args.numbers if isFloat(x)])
+args.primary = numpy.array([float(x) for x in args.primary if isFloat(x)])
+args.others = numpy.array([float(x) for x in args.others if isFloat(x)])
 
-if (len(numbers) == 0):
+if pValue and (len(args.primary) <= 1 or len(args.others) <= 1):
+    raise Exception('number sets too small to calculate p-value statistics!')
+    exit(1)
+
+if (len(args.primary) == 0):
     exit(1)
 
 results = []
-labels = None if args.quiet else []
+labels = []
 
-for f in args.properties:
-    f = f.lower()
-    parm = None
-
-    if f[0] == 'p' and isFloat(f[1:]):
-        parm = f[1:]
-        f = 'p%'
-
-    if f not in props:
-        raise Exception(f"Could not find property '{f}'")
-
-    if not args.quiet:
-        if parm is None:
-            labels.append(props[f]['label'])
-        else:
-            labels.append(props[f]['label'].replace('%', parm))
-
-    if parm is None:
-        results.append(props[f]['func'](numbers))
+for p in args.properties:
+    if p[0] == 'p' and isFloat(p[1:]):
+        percentile = p[1:]
+        p = 'p%'
+        labels.append(props[p]['label'].replace('%', percentile))
+        results.append(props[p]['func'](args.primary, float(percentile)))
     else:
-        results.append(props[f]['func'](numbers, float(parm)))
+        if p not in props:
+            raise Exception(f"Could not find property '{p}'")
+        labels.append(props[p]['label'])
+        if p == 'pval':
+            results.append(props[p]['func'](args.primary, args.others))
+        else:
+            results.append(props[p]['func'](args.primary))
 
 resultFormat = '{:}'
 

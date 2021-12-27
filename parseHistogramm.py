11a6c0bdfd56665d0f64eb1cd600fbacb87886b0
Björn Gottschall <github.mail@bgottschall.de>
2021-12-27 15:03:35 +0100

    * added slice to parseHistogramm

--- parseHistogramm.py
+++ parseHistogramm.py
@@ -52,7 +52,7 @@ parser.add_argument("input", nargs="?", help="compressed histogramm csv")
 parser.add_argument("--filter-columns", default=[], type=SliceType(), nargs='*', help='filter based on these columns')
 parser.add_argument("--filter-mode", choices=['any', 'all'], default='any', help='either a value must match in any of the columns of all columns must contain a filter value')
 parser.add_argument("--filter-data", default=[], type=str, nargs='*', help='filter based on this data')
-parser.add_argument("--slice", type=SliceType(), default=SliceType()('1:'), help="slice histogram (default '1:')",)
+parser.add_argument("--slice", type=str, default='1:', help="slice histogram (default '1:')",)
 parser.add_argument("--delimiter", help="csv delimiter (default '%(default)s')", default=';')
 parser.add_argument("--flatten", choices=['buckets', 'counts', 'items'], help="output flat histogramm", default=[], nargs="*")
 parser.add_argument("--items", nargs="*", help="select items", default=False)
@@ -68,6 +68,14 @@ if args.input and not os.path.exists(args.input):
 
 if (len(args.filter_columns) > 0 and len(args.filter_data) == 0) or (len(args.filter_columns) == 0 and len(args.filter_data) > 0):
     raise Exception('Filtering requires --filter-columns and --filter-data!')
+
+if args.slice.isnumeric():
+    args.slice = [int(args.slice), int(args.slice) + 1]
+else:
+    args.slice = [int(x) if x.isnumeric() else None for x in args.slice.split(':')]
+    if len(args.slice) != 2:
+        raise Exception('Invalid histogram slice')
+    args.slice[0] = args.slice[0] if args.slice[0] is not None else 0
 
 if not args.input:
     try:
@@ -86,69 +94,80 @@ for header in csvFile:
         continue
     break
 
-
 if header is None:
     raise Exception('Could not find a histogram header!')
 
-selector = None
+args.slice[1] = args.slice[1] if args.slice[1] is not None else len(header)
+if args.slice[0] == args.slice[1]:
+    raise Exception('Invalid histogram slice range')
 
-if args.buckets:
-    selector = [0] + [i for i, x in enumerate(header) if i > 0 and x in args.buckets]
+hasItems = args.slice[0] > 0
 
-if 'buckets' in args.flatten and not all(isFloat(x) for x in header[args.slice]):
+if args.items and not hasItems:
+    raise Exception('Cannot select items when histogram slice starts at 0')
+
+
+if 'buckets' in args.flatten and not all(isFloat(x) for x in header[slice(*args.slice)]):
     raise Exception('Flatten buckets only works with numeric header')
 
-hasItems = 0 not in (args.slice.indices(1) if isinstance(args.slice, slice) else [args.slice])
+
+selector = None
+if args.buckets:
+    selector = [i for i, x in enumerate(header[slice(*args.slice)]) if x in args.buckets]
 
 outputFile = sys.stdout if not args.output else xopen.xopen(args.output, 'w')
 
-if len(args.flatten) == 0 or 'items' in args.flatten and not any(x in args.flatten for x in ['buckets', 'counts']):
-    outputFile.write(args.delimiter.join([header[0] if hasItems else ''] + header[args.slice]) + '\n')
-else:
+if len(args.flatten) == 0:
+    outputFile.write(args.delimiter.join(header) + '\n')
+elif 'items' not in args.flatten and any(x in args.flatten for x in ['counts', 'buckets']):
+    outputFile.write(args.delimiter.join(header[:args.slice[0]] + [x for x in ['counts', 'buckets'] if x in args.flatten]) + '\n')
+elif 'items' in args.flatten and any(x in args.flatten for x in ['counts', 'buckets']):
     outputFile.write(args.delimiter.join([header[0] if hasItems else ''] + [x for x in ['counts', 'buckets'] if x in args.flatten]) + '\n')
+else:
+    outputFile.write(args.delimiter.join([header[0] if hasItems else ''] + header[slice(*args.slice)]) + '\n')
 
-flatHist = [0] * (len(header[args.slice]) - 1)
+flatHist = [0] * len(header[slice(*args.slice)])
 flat = {
     'buckets': 0,
     'counts': 0
 }
 
 
-def parseNormal(line):
-    outputFile.write(args.delimiter.join([line[0] if hasItems else str(i)] + line[args.slice]) + '\n')
+def parseNormal(line, itemHeaders, itemValues):
+    outputFile.write(args.delimiter.join(line[:args.slice[0]] + itemValues + line[args.slice[1]:]) + '\n')
 
 
-def parseCounts(line):
-    outputFile.write(args.delimiter.join([line[0] if hasItems else str(i), str(sum([float(i) for i in line[args.slice] if len(i) > 0]))]) + '\n')
+def parseCounts(line, itemHeaders, itemValues):
+    outputFile.write(args.delimiter.join(line[:args.slice[0]] + [str(sum([float(i) for i in itemValues if len(i) > 0]))] + line[args.slice[1]:]) + '\n')
 
 
-def parseBuckets(line):
-    outputFile.write(args.delimiter.join([line[0] if hasItems else str(i), str(sum([float(h) * float(i) for (h, i) in zip(header[args.slice], line[args.slice]) if len(i) > 0]))]) + '\n')
+def parseBuckets(line, itemHeaders, itemValues):
+    outputFile.write(args.delimiter.join(line[:args.slice[0]] + [str(sum([float(h) * float(i) for (h, i) in zip(itemHeaders, itemValues) if len(i) > 0]))] + line[args.slice[1]:]) + '\n')
 
 
-def parseCountsBuckets(line):
-    outputFile.write(args.delimiter.join([line[0] if hasItems else str(i), str(sum([float(i) for i in line[args.slice] if len(i) > 0])), str(sum([float(h) * float(i) for (h, i) in zip(header[args.slice], line[args.slice]) if len(i) > 0]))]) + '\n')
+def parseCountsBuckets(line, itemHeaders, itemValues):
+    outputFile.write(args.delimiter.join(line[:args.slice[0]] + [str(sum([float(i) for i in itemValues if len(i) > 0])), str(sum([float(h) * float(i) for (h, i) in zip(itemHeaders, itemValues) if len(i) > 0]))] + line[args.slice[1]:]) + '\n')
 
 
-def parseItems(line):
+def parseItems(line, itemHeaders, itemValues):
     global flatHist
-    flatHist = [(p + float(v) if len(v) > 0 else p) for (p, v) in zip(flatHist, line[args.slice])]
+    flatHist = [(p + float(v) if len(v) > 0 else p) for (p, v) in zip(flatHist, itemValues)]
 
 
-def parseItemsBuckets(line):
+def parseItemsBuckets(line, itemHeaders, itemValues):
     global flat
-    flat['buckets'] += sum([float(h) * float(i) for (h, i) in zip(header[args.slice], line[args.slice]) if len(i) > 0])
+    flat['buckets'] += sum([float(h) * float(i) for (h, i) in zip(itemHeaders, itemValues) if len(i) > 0])
 
 
-def parseItemsCounts(line):
+def parseItemsCounts(line, itemHeaders, itemValues):
     global flat
-    flat['counts'] += sum([float(i) for i in line[args.slice] if len(i) > 0])
+    flat['counts'] += sum([float(i) for i in itemValues if len(i) > 0])
 
 
-def parseItemsCountsBuckets(line):
+def parseItemsCountsBuckets(line, itemHeaders, itemValues):
     global flat
-    flat['counts'] += sum([float(i) for i in line[args.slice] if len(i) > 0])
-    flat['buckets'] += sum([float(h) * float(i) for (h, i) in zip(header[args.slice], line[args.slice]) if len(i) > 0])
+    flat['counts'] += sum([float(i) for i in itemValues if len(i) > 0])
+    flat['buckets'] += sum([float(h) * float(i) for (h, i) in zip(itemHeaders, itemValues) if len(i) > 0])
 
 
 if 'items' in args.flatten:
@@ -157,16 +176,16 @@ if 'items' in args.flatten:
     elif 'counts' in args.flatten:
         parser = parseItemsCounts
     elif 'buckets' in args.flatten:
-        parser = parseItemsCounts
+        parser = parseItemsBuckets
     else:
         parser = parseItems
 else:
     if all(x in args.flatten for x in ['counts', 'buckets']):
         parser = parseCountsBuckets
-    elif 'buckets' in args.flatten:
-        parser = parseBuckets
     elif 'counts' in args.flatten:
         parser = parseCounts
+    elif 'buckets' in args.flatten:
+        parser = parseBuckets
     else:
         parser = parseNormal
 
@@ -174,6 +193,9 @@ else:
 applyFilter = len(args.filter_columns) > 0
 filterSlices = [s if isinstance(s, slice) else slice(s, s + 1) for s in args.filter_columns]
 filterFunc = any if args.filter_mode == 'any' else all
+itemHeaders = header[slice(*args.slice)]
+if selector is not None:
+    itemHeaders = [itemHeaders[i] for i in selector]
 
 for i, line in enumerate(csvFile):
     if line[0].startswith('#'):
@@ -186,10 +208,11 @@ for i, line in enumerate(csvFile):
     if applyFilter and not filterFunc(v in args.filter_data for lv in [line[slc] for slc in filterSlices] for v in lv):
         continue
 
+    itemValues = line[slice(*args.slice)]
     if selector is not None:
-        line = [line[i] for i in selector]
+        itemValues = [itemValues[i] for i in selector]
 
-    parser(line)
+    parser(line, itemHeaders, itemValues)
 
 if 'items' in args.flatten:
     if not any(x in args.flatten for x in ['counts', 'buckets']):
